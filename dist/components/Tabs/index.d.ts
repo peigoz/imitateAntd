d617e94ac4faf3d66e46ef15b38be2f4338b55aa
candane <candane@126.com>
2021-03-08 03:37:29 +0800

    Delete dist directory

--- dist/components/Tabs/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react';
-import { TabsProps } from './Tabs';
-import { TabItemProps } from './TabItem';
-export declare type ITabsComponent = FC<TabsProps> & {
-    Item: FC<TabItemProps>;
-};
-declare const TransTabs: ITabsComponent;
-export default TransTabs;
