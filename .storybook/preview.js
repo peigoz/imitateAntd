507c8bd34e48bddb1f74683a493cadec7842a77b
candane <candane@126.com>
2021-03-08 03:37:15 +0800

    Delete .storybook directory

--- .storybook/preview.js
+++
@@ -1,8 +0,0 @@
-import '../src/styles/index.scss';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' }
-}
